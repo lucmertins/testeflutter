f9b7253ebc48e4eed6bc1445204d7cdecadc0857
Luciano Edson Mertins <luciano@mertins.com.br>
2019-06-05 08:25:07 -0300

    Adicionando touch conforme https://codelabs.developers.google.com/codelabs/first-flutter-app-pt2/#4

--- lib/main.dart
+++ lib/main.dart
@@ -50,10 +50,18 @@ class RandomWordsState extends State<RandomWords> {
         style: _biggerFont,
       ),
       trailing: Icon(
-        // Add the lines from here...
         alreadySaved ? Icons.favorite : Icons.favorite_border,
         color: alreadySaved ? Colors.red : null,
       ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
     );
   }
 }
